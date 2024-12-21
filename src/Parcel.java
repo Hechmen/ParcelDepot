50c514b7059d3dd8c204941fbc86e150329faaff
Hechmen <bashikov2014@gmail.com>
2024-12-21 20:11:33 +0000

    Parcel and Status

--- src/Parcel.java
+++ src/Parcel.java
@@ -1,3 +1,42 @@
 public class Parcel {
+    private String ID;
+    private float weight;
+    private int width;
+    private int length;
+    private int height;
+    private int daysInDepot;
+//    private Status status;
+
+    public Parcel(String ID, float weight, int width, int length, int height, int daysInDepot) {
+        this.ID = ID;
+        this.weight = weight;
+        this.width = width;
+        this.length = length;
+        this.height = height;
+        this.daysInDepot = daysInDepot;
+//        this.status = status;
+    }
+
+    public String getID() {
+        return ID;
+    }
+    public float getWeight() {
+        return weight;
+    }
+    public int getWidth() {
+        return width;
+    }
+    public int getLength() {
+        return length;
+    }
+    public int getHeight() {
+        return height;
+    }
+    public int getDaysInDepot() {
+        return daysInDepot;
+    }
+//    public enum getStatus() {
+//        return status;
+//    }
 
 }
--- src/Status.java
+++ src/Status.java
@@ -1,2 +1,12 @@
-package PACKAGE_NAME;public class Status {
+public enum Status {
+    COLLECTED(" Collected"), UNCOLLECTED(" Uncollected");
+    private String status;
+
+    private Status(String status) {
+        this.status = status;
+    }
+
+    public String toString() {
+        return status;
+    }
 }
